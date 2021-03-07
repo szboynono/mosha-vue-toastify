8390df45635c18536bf436d5b9e96abc69883ff1
szboynono <szboylbd@hotmail.com>
2021-03-07 14:51:36 -0500

    initilize

---
+++ lib/main.ts
@@ -0,0 +1,3 @@
+export const sum = (a: any, b: any) => {
+  console.log(a+b);
+}
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, '/lib/main.ts'),
+      name: 'mosha-vue-toastify'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+})
