fdaeee35df5fdb5ec8f450e3f988269c442765f3
szboynono <szboylbd@hotmail.com>
2021-03-14 01:56:53 -0500

    fix: now it can import styles

--- examples/vite-sample/src/main.ts
+++ examples/vite-sample/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import moshaToast from 'mosha-vue-toastify'
-
+import 'mosha-vue-toastify/dist/style.css'
 createApp(App).use(moshaToast).mount('#app')
