1713d26123242603b18714b75eadc07f8199e636
szboynono <szboylbd@hotmail.com>
2021-03-14 00:04:26 -0500

    feat: add example

---
+++ examples/vite-sample/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="hit">hit</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+export default defineComponent({
+  name: 'HelloWorld',
+  setup: () => {
+    const moshaToast = inject<any>('moshaToast')
+    const hit = () => {
+      moshaToast()
+    }
+    return { hit }
+  }
+})
+</script>
---
+++ examples/vite-sample/src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import moshaToast from 'mosha-vue-toastify'
+
+createApp(App).use(moshaToast).mount('#app')
