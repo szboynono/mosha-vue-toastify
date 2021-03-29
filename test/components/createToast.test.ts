5318cc797250dde30f87d1e8017bf1c3053e5f73
szboynono <szboylbd@hotmail.com>
2021-03-29 00:04:39 -0400

    add a test for initialization

--- test/components/createToast.test.ts
+++ test/components/createToast.test.ts
@@ -1,3 +1,17 @@
-test('should pass', () => {
-  expect(1).toBe(1);
+import { ToastOptions } from '../../lib/types';
+import { initializeOptions } from '../../lib/components/createToast'
+
+test('initializeOptions should initilize the options', () => {
+  const options: ToastOptions = {}
+  const result = initializeOptions(options)
+  expect(result).toEqual({
+    hideProgressBar: false,
+    position: "top-right",
+    showCloseButton: undefined,
+    showIcon: undefined,
+    swipeClose: undefined,
+    timeout: 5000,
+    transition: "bounce",
+    type: "default",
+  })
 })
